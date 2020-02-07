d20ab2f410e82213643e32d0e69e608e24c302c5
Dmitry <fanrok@mail.ru>
2020-02-07 23:54:45 +0300

    bag fix 3

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -13,13 +13,23 @@ fun Activity.hideKeyboard() {
 fun Activity.isKeyboardOpen(): Boolean {
     val r = Rect()
     this.window.decorView.getWindowVisibleDisplayFrame(r)
-
     val heightDiff: Int = this.window.decorView.height - r.height()
-    val check = Math.round(this.convertDpToPx(50F))
-    return heightDiff > check
+    return heightDiff > 0
 }
 
 
 fun Activity.isKeyboardClosed(): Boolean {
     return !this.isKeyboardOpen()
 }
+
+//fun Activity.isKeyboardOpen(): Boolean {
+//    val visibleBounds = Rect()
+//    this.getRootView().getWindowVisibleDisplayFrame(visibleBounds)
+//    val heightDiff = getRootView().height - visibleBounds.height()
+//    val marginOfError = Math.round(this.convertDpToPx(50F))
+//    return heightDiff > marginOfError
+//}
+//
+//fun Activity.isKeyboardClosed(): Boolean {
+//    return !this.isKeyboardOpen()
+//}
