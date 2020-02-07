412b29efb99a4fa9cff6ae5e8dc34100cf98b93f
Dmitry <fanrok@mail.ru>
2020-02-07 23:42:25 +0300

    bag fix 2

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -5,29 +5,21 @@ import android.content.Context
 import android.graphics.Rect
 import android.view.inputmethod.InputMethodManager
 
-fun Activity.hideKeyboard(){
+fun Activity.hideKeyboard() {
     val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(this.getCurrentFocus()?.getWindowToken(), 0)
 }
 
-fun Activity.isKeyboardOpen (): Boolean{
+fun Activity.isKeyboardOpen(): Boolean {
     val r = Rect()
     this.window.decorView.getWindowVisibleDisplayFrame(r)
 
-    val heightDiff:Int = this.window.decorView.height - (r.bottom - r.top)
-    if (heightDiff > 100) { // я не нашел способа получше
-        return true
-    }
-    return false
+    val heightDiff: Int = this.window.decorView.height - r.height()
+    val check = Math.round(this.convertDpToPx(50F))
+    return heightDiff > check
 }
 
-fun Activity.isKeyboardClosed(): Boolean{
-    val r = Rect()
-    this.window.decorView.getWindowVisibleDisplayFrame(r)
 
-    val heightDiff:Int = this.window.decorView.height - (r.bottom - r.top)
-    if (heightDiff > 100) {
-        return false
-    }
-    return true
+fun Activity.isKeyboardClosed(): Boolean {
+    return !this.isKeyboardOpen()
 }
