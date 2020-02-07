0f759e7d5e3dfc8623d17735150e3c1a49ed6122
Dmitry <fanrok@mail.ru>
2020-02-08 00:41:50 +0300

    bag fix 5

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.graphics.Rect
 import android.view.inputmethod.InputMethodManager
+import kotlin.math.roundToInt
 
 fun Activity.hideKeyboard() {
     val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -14,7 +15,7 @@ fun Activity.isKeyboardOpen(): Boolean {
     val r = Rect()
     this.window.decorView.getWindowVisibleDisplayFrame(r)
     val heightDiff: Int = this.window.decorView.height - r.height()
-    val check = Math.round(this.convertDpToPx(50F))
+    val check = this.convertDpToPx(50F).roundToInt()
     return heightDiff > check
 }
 
